3427514ac71778fe3d357d54eab49aa64be80b93
Talha Oktay <talhaoktay21@istanbularel.edu.tr>
2025-12-05 01:02:43 +0300

    Kart 10: ReservationStatus enum eklendi (Duzeltme)

--- src/ReservationStatus.java
+++ src/ReservationStatus.java
@@ -3,7 +3,7 @@
  * Rezervasyonun güncel durumunu belirtir.
  */
 public enum ReservationStatus {
-    ACTIVE,     // Rezervasyon geçerli
+    ACTIVE,     // // Aktif rezervasyon Rezervasyon geçerli
     CANCELLED,  // Rezervasyon iptal edilmiş
     COMPLETED   // Konaklama tamamlanmış
 }
